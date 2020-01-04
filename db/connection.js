7049a7be5cf4c45f21478c6406ea85280c224d23
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-01-04 19:16:03 +0000

    added initial questions

--- db/connection.js
+++ db/connection.js
@@ -5,28 +5,91 @@ const express = require("express");
 const inquirer = require("inquirer");
 const figlet = require("figlet");
 
-// init();
-figlet("Employee Tracker", function(err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
-
-var connection = mysql.createConnection({
+const dbConfig = {
   host: "localhost",
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "company"
-});
+};
 
-connection.connect();
+async function init() {
+  renderAppTitle();
 
-connection.query("SELECT * FROM employee", function(error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-});
+  const choice = await displayQuestions();
+  handleChoice(choice);
+}
 
-connection.end();
+function renderAppTitle() {
+  console.log(figlet.textSync("Employee Tracker"));
+}
+
+function handleChoice(choice) {
+  switch (choice) {
+    case "View all employees":
+      displayEmployees();
+      break;
+
+    case "View all departments":
+      displayDepartments();
+      break;
+
+    case "View all roles":
+      displayRoles();
+      break;
+    default:
+      return;
+  }
+}
+
+function displayEmployees() {
+  const connection = mysql.createConnection(dbConfig);
+
+  connection.connect();
+
+  connection.query("SELECT * FROM employee", async function(error, results) {
+    if (error) throw error;
+    console.table(results);
+  });
+
+  connection.end();
+}
+
+function displayDepartments() {
+  const connection = mysql.createConnection(dbConfig);
+
+  connection.connect();
+
+  connection.query("SELECT * FROM department", async function(error, results) {
+    if (error) throw error;
+    console.table(results);
+  });
+
+  connection.end();
+}
+
+function displayRoles() {
+  const connection = mysql.createConnection(dbConfig);
+
+  connection.connect();
+
+  connection.query("SELECT * FROM role", async function(error, results) {
+    if (error) throw error;
+    console.table(results);
+  });
+
+  connection.end();
+}
+
+async function displayQuestions() {
+  const { choices } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choices",
+      message: `What would you like to do?`,
+      choices: ["View all employees", "View all departments", "View all roles"]
+    }
+  ]);
+  return choices;
+}
+
+init();
---
+++ db/index.js
@@ -0,0 +1,29 @@
+// Build a command-line application that at a minimum allows the user to:
+
+// Add departments, roles, employees
+//INSERT INTO department (id, name)
+// INSERT INTO role (id, department_id, title, salary)
+// INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+
+// View departments, roles, employees
+//SELECT * FROM
+
+// Update employee roles
+//UPDATE employee
+// SET role_id = 7;
+// WHERE first_name = "Heather", last_name = "Oak"
+
+// Bonus points if you're able to:
+
+// Update employee managers
+
+// View employees by manager
+// SELECT column_name(s)
+// FROM table1
+// LEFT JOIN table2
+// ON table1.column_name = table2.column_name;
+
+// Delete departments, roles, and employees
+//DELETE FROM department WHERE name = "Sales"
+
+// View the total utilized budget of a department -- ie the combined salaries of all employees in that department//
